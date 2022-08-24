e8622a60ecec647cb693d3bd3679204cf34c4e11
junho jang <cclemon3248@gmail.com>
2022-08-24 11:55:58 +0900

    修正

--- memo.rb
+++ memo.rb
@@ -1,11 +1,11 @@
 require "csv"
  
 puts "1(新規でメモを作成) 2(既存のメモ編集する)"
-memo_type = gets.to_s.chomp
+memo_type = gets.chomp
 
 if memo_type == "1"
   puts "拡張子を除いたファイルを入力してください"
-  file_name = gets.to_s.chomp
+  file_name = gets.chomp
 
   puts "メモしたい内容を記入してください"
   puts "完了したらCtrl + Dをおします"
@@ -21,7 +21,7 @@ if memo_type == "1"
 
 elsif memo_type == "2"
   puts "拡張子を除いたファイルを入力してください"
-  file_name = gets.to_s.chomp
+  file_name = gets.chomp
 
   puts "編集内容を記入してください"
   puts "完了したらCtrl + Dをおします"
